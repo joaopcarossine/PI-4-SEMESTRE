8bf7a4b8445bb2cc36fcd9020a81b1e3021b5a91
João Carossine <joao.carossine@fatec.sp.gov.br>
2025-10-05 22:34:46 -0300

    🎉 INICIAL
    COMMIT INICIAL COM ESTRUTURA MVT/STATIC/REQUIREMENTS

---
+++ FLUXO_APROVACAO/FLUXO_APROVACAO/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for FLUXO_APROVACAO project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FLUXO_APROVACAO.settings')
+
+application = get_wsgi_application()
